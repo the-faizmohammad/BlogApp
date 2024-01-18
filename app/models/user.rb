9744415ac7145962a68158a424d41bb4661f1bd1
user-faiz <email2faizm@gmail.com>
2024-01-19 00:54:07 +0530

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
-end
+    has_many :posts, foreign_key: 'author_id'
+    has_many :comments
+    has_many :likes
+  
+    def three_recent_posts
+      posts.where(author: self).order(created_at: :desc).limit(3)
+    end
+  end
+  
