76b65a674b817611ef30ba3db899f169e125cd0a
user-faiz <email2faizm@gmail.com>
2024-01-19 00:56:22 +0530

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,13 @@
 class Like < ApplicationRecord
-end
+    belongs_to :post
+    belongs_to :user
+  
+    after_save :update_likes_counter
+  
+    private
+  
+    def update_likes_counter
+      post.increment!(:likes_counter)
+    end
+  end
+  
