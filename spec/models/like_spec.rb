c363320c8c08914ad7f9e1d23e536f35412bb46c
user-faiz <email2faizm@gmail.com>
2024-01-19 18:10:34 +0530

    fixed linters error

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Like, type: :model do
   it 'is valid with a user and post' do
     user = User.create(name: 'Liker')
     post = Post.create(title: 'Test Post', text: 'This is a test post.', author: user)
-    like = Like.new(user: user, post: post)
+    like = Like.new(user:, post:)
     expect(like).to be_valid
   end
 
   it 'is invalid without a user' do
     post = Post.create(title: 'Test Post', text: 'This is a test post.')
-    like = Like.new(post: post)
+    like = Like.new(post:)
     expect(like).to_not be_valid
   end
 
   it 'is invalid without a post' do
     user = User.create(name: 'Liker')
-    like = Like.new(user: user)
+    like = Like.new(user:)
     expect(like).to_not be_valid
   end
 end
