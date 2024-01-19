53ae9dcdd757b95ebdb49624c5d83762ec2293f6
user-faiz <email2faizm@gmail.com>
2024-01-19 17:20:29 +0530

    Add test for Like model

---
+++ spec/like_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  it 'is valid with a user and post' do
+    user = User.create(name: 'Liker')
+    post = Post.create(title: 'Test Post', text: 'This is a test post.', author: user)
+    like = Like.new(user: user, post: post)
+    expect(like).to be_valid
+  end
+
+  it 'is invalid without a user' do
+    post = Post.create(title: 'Test Post', text: 'This is a test post.')
+    like = Like.new(post: post)
+    expect(like).to_not be_valid
+  end
+
+  it 'is invalid without a post' do
+    user = User.create(name: 'Liker')
+    like = Like.new(user: user)
+    expect(like).to_not be_valid
+  end
+end
