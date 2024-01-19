140fdde22a10c7b4e99986acbe21d4ea12004cca
user-faiz <email2faizm@gmail.com>
2024-01-19 17:06:13 +0530

    Update model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
-
-  def three_recent_posts
-    posts.where(author: self).order(created_at: :desc).limit(3)
+    has_many :posts, foreign_key: 'author_id'
+    has_many :comments
+    has_many :likes
+  
+    validates :name, presence: true
+    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+    def three_recent_posts
+      posts.where(author: self).order(created_at: :desc).limit(3)
+    end
   end
-end
