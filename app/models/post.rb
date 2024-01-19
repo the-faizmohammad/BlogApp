936a725181131040b5f48974aef60f0532244fe9
user-faiz <email2faizm@gmail.com>
2024-01-19 16:31:15 +0530

    Apply Post Validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-  after_save :update_posts_counter
-
-  def five_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+    after_save :update_posts_counter
+  
+    validates :title, presence: true, length: { maximum: 250 }
+    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+    def five_recent_comments
+      comments.where(post_id: id).order(created_at: :desc).limit(5)
+    end
+  
+    private
+  
+    def update_posts_counter
+      author.increment!(:posts_counter)
+    end
   end
-
-  private
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
-end
