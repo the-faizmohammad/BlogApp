b1b9c7df14a0eb6ad6f584db54d291eafdba5cea
user-faiz <email2faizm@gmail.com>
2024-01-25 00:10:44 +0530

    update controller logic for posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
-  def index
-    # Add placeholder HTML here
+    layout 'boilerplate'
+    def index
+      @user = User.find(params[:user_id])
+      @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
+      @posts = @posts.paginate(page: params[:page], per_page: 2)
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
   end
-
-  def show
-    # Add placeholder HTML here
-  end
-end
