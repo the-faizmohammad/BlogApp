db3a21786f96002502fd6accb1e6c8813745899a
user-faiz <email2faizm@gmail.com>
2024-01-27 01:04:25 +0530

    fixed linters error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,13 @@
-class CommentsController < ApplicationController
+class LikesController < ApplicationController
   layout 'boilerplate'
-
-  def new
-    @comment = Comment.new
-  end
-
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user = current_user
-
-    if save_comment
-      flash[:success] = 'Your Comment was added successfully!'
-      redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
+    @like = Like.new(post_id: params[:post_id], user_id: params[:user_id])
+    @like.user = current_user
+    if @like.save
+      flash[:success] = 'Liked the Post!'
+      redirect_to user_post_path(id: @like.post_id, user_id: @like.user_id)
     else
-      flash.now[:error] = 'Error: Try Agai ! Comment could not be saved!'
-      render :new, locals: { comment: @comment }
+      flash.now[:error] = 'Like to the Post could not be saved!'
     end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text, :user_id, :post_id)
-  end
-
-  def save_comment
-    @comment.save
   end
 end
