94926d570a427ae16dc7e505ac6bf49b0def753f
user-faiz <email2faizm@gmail.com>
2024-01-27 00:52:18 +0530

    fixed linters error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,30 @@
 class CommentsController < ApplicationController
-    layout 'boilerplate'
-  
-    def new
-      @comment = Comment.new
-    end
-  
-    def create
-      @comment = Comment.new(comment_params)
-      @comment.user = current_user
-  
-      if save_comment
-        flash[:success] = 'Your Comment was added successfully!'
-        redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
-      else
-        flash.now[:error] = 'Error: Try Agai ! Comment could not be saved!'
-        render :new, locals: { comment: @comment }
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text, :user_id, :post_id)
-    end
-  
-    def save_comment
-      @comment.save
+  layout 'boilerplate'
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+
+    if save_comment
+      flash[:success] = 'Your Comment was added successfully!'
+      redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
+    else
+      flash.now[:error] = 'Error: Try Agai ! Comment could not be saved!'
+      render :new, locals: { comment: @comment }
     end
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :user_id, :post_id)
+  end
+
+  def save_comment
+    @comment.save
+  end
+end
