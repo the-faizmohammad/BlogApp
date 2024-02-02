f071ab6d9c44cf31d5e3d2df85dffbca3ee93e70
user-faiz <email2faizm@gmail.com>
2024-02-03 02:47:07 +0530

    update schema

--- db/migrate/20240202201251_add_unique_index_to_users_email.rb
+++ db/migrate/20240202201251_add_unique_index_to_users_email.rb
@@ -1,5 +1,10 @@
 class AddUniqueIndexToUsersEmail < ActiveRecord::Migration[7.1]
-  def change
-    add_index :users, :email, unique: true
+  def up
+    remove_index :users, :email if index_exists?(:users, :email)
+    add_index :users, :email, if_not_exists: true
+  end
+
+  def down
+    remove_index :users, :email if index_exists?(:users, :email)
   end
 end
