1cfb5d6869526148a19eea5329548528f5e3e083
user-faiz <email2faizm@gmail.com>
2024-02-07 21:50:45 +0530

    update Schema.rb, user.rb

--- db/migrate/20240202213724_add_devise_to_users.rb
+++ db/migrate/20240202213724_add_devise_to_users.rb
@@ -4,41 +4,47 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email, null: false, default: "", unless: -> { column_exists?(:users, :email) }
-      t.string :encrypted_password, null: false, default: "", unless: -> { column_exists?(:users, :encrypted_password) }
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token, unless: -> { column_exists?(:users, :reset_password_token) }
-      t.datetime :reset_password_sent_at, unless: -> { column_exists?(:users, :reset_password_sent_at) }
+      unless column_exists?(:users, :reset_password_token)
+        t.string   :reset_password_token
+        t.datetime :reset_password_sent_at
+      end
 
       ## Rememberable
-      t.datetime :remember_created_at, unless: -> { column_exists?(:users, :remember_created_at) }
+      t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false, unless: -> { column_exists?(:users, :sign_in_count) }
-      t.datetime :current_sign_in_at, unless: -> { column_exists?(:users, :current_sign_in_at) }
-      t.datetime :last_sign_in_at, unless: -> { column_exists?(:users, :last_sign_in_at) }
-      t.string   :current_sign_in_ip, unless: -> { column_exists?(:users, :current_sign_in_ip) }
-      t.string   :last_sign_in_ip, unless: -> { column_exists?(:users, :last_sign_in_ip) }
+      unless column_exists?(:users, :sign_in_count)
+        t.integer  :sign_in_count, default: 0, null: false
+        t.datetime :current_sign_in_at
+        t.datetime :last_sign_in_at
+        t.string   :current_sign_in_ip
+        t.string   :last_sign_in_ip
+      end
 
       ## Confirmable
-      t.string   :confirmation_token, unless: -> { column_exists?(:users, :confirmation_token) }
-      t.datetime :confirmed_at, unless: -> { column_exists?(:users, :confirmed_at) }
-      t.datetime :confirmation_sent_at, unless: -> { column_exists?(:users, :confirmation_sent_at) }
-      t.string   :unconfirmed_email, unless: -> { column_exists?(:users, :unconfirmed_email) }
+      unless column_exists?(:users, :confirmation_token)
+        t.string   :confirmation_token
+        t.datetime :confirmed_at
+        t.datetime :confirmation_sent_at
+        t.string   :unconfirmed_email
+      end
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      # Uncomment below if timestamps were not included in your original model.
-      t.timestamps null: false, unless: -> { column_exists?(:users, :created_at) }
+      ## Uncomment below if timestamps were not included in your original model.
+      t.timestamps null: false unless column_exists?(:users, :created_at)
     end
 
-    add_index :users, :email, unique: true, unless: -> { index_exists?(:users, :email) }
-    add_index :users, :reset_password_token, unique: true, unless: -> { index_exists?(:users, :reset_password_token) }
-    add_index :users, :confirmation_token, unique: true, unless: -> { index_exists?(:users, :confirmation_token) }
+    add_index :users, :email, unique: true unless index_exists?(:users, :email)
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token)
+    add_index :users, :confirmation_token, unique: true unless index_exists?(:users, :confirmation_token)
     # add_index :users, :unlock_token,         unique: true
   end
 
