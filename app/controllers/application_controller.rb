f1a22b15186c29fcd6f55f6050c1647c981d0163
user-faiz <email2faizm@gmail.com>
2024-02-09 18:08:54 +0530

    Update Linters error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
-# adding device parameters
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
-
-
-
-  
 end
