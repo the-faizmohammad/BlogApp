f6a71cf5bbe1a8e95876da02a902508870bf6a40
Reem <reem.mohamed.abdelfatah@gmail.com>
2024-02-09 14:31:49 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::Base
 # adding device parameters
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
