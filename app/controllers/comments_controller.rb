f8ec44a4540677c18f2eabd7f4596a03eeb468d7
user-faiz <email2faizm@gmail.com>
2024-02-09 19:08:16 +0530

    update comments controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   layout 'boilerplate'
   def new
     @comment = Comment.new
@@ -14,6 +15,14 @@ class CommentsController < ApplicationController
       flash.now[:error] = 'Error: Comment could not be saved!'
       render :new, locals: { comment: @comment }
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.post.decrement!(:comments_counter)
+    @comment.destroy!
+    flash[:success] = 'Comment deleted.'
+    redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
   end
 
   private
