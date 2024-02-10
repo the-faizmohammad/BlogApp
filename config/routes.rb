026f51c4392ffa6138950f57a07b59de938c70a9
user-faiz <email2faizm@gmail.com>
2024-02-10 12:01:09 +0530

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    resources :users, only: [:index] do
+      resources :posts, only: [:index]
+    end
+    resources :posts, only: [:show] do
+      resources :comments, only: [:index]
+    end
+  end
 end
