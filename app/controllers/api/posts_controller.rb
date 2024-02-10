f37fbc1ac60db3c609f6e9a8e5ee9ac3f977a12d
user-faiz <email2faizm@gmail.com>
2024-02-10 12:44:24 +0530

    fixed liners error

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,24 +1,24 @@
 class Api::PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts
-      render json: @posts
-    end
-
-    def show
-      @post = Post.find(params[:post_id])
-      render json: @post
-    end
-
-    def create_comment
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(comment_params)
-      render json: @comment, status: :created
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    render json: @posts
   end
+
+  def show
+    @post = Post.find(params[:post_id])
+    render json: @post
+  end
+
+  def create_comment
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    render json: @comment, status: :created
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
