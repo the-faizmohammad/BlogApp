aaef60521a32988bd30ce19bacb3765baf43c40f
user-faiz <email2faizm@gmail.com>
2024-02-10 12:38:57 +0530

    Add api end points for comment and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
   has_many :likes
+
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
@@ -9,7 +11,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def five_recent_comments
-    comments.where(post_id: id).order(created_at: :desc).limit(5)
+    comments.order(created_at: :desc).limit(5)
   end
 
   private
